120ddc27ad5f7dede753f0e6148208846362d203
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2018-01-19 15:11:34 +0100

    built a house with opening doors

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor* owner = GetOwner();
+	FRotator newRotation = FRotator(0, 20, 0);
+	owner->SetActorRotation(newRotation);
+	//FString objectPos = GetOwner()->GetTransform().GetRotation();
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
